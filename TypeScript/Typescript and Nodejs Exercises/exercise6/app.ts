a2638d1af98c5b99f32165f2d10bf23342b5cae6
Muaz <muazgulfam7890@gmail.com>
2024-02-19 12:04:15 +0500

    in context of ex8 added some comments in exercise 6 and 7

--- TypeScript/Typescript and Nodejs Exercises/exercise6/app.ts
+++ TypeScript/Typescript and Nodejs Exercises/exercise6/app.ts
@@ -1,3 +1,3 @@
-let myName = "\n\tMUAZ BIN GULFAM\t\n"
+let myName = "\n\tMUAZ BIN GULFAM\t\n"; //"\n" and "\t" are known as escape characters which performs the respected operations even in the inverted commas.
 console.log(myName);
-console.log("Name without White spaces =>" + myName.trim())
+console.log("Name without White spaces =>" + myName.trim()) //".trim() function removes white spaces"
--- TypeScript/Typescript and Nodejs Exercises/exercise7/numberEight.ts
+++ TypeScript/Typescript and Nodejs Exercises/exercise7/numberEight.ts
@@ -1,8 +1,8 @@
-let additionResult: number = 2 + 6;
+let additionResult: number = 2 + 6; //Created a variable of type number named as "additionResult" and store a sum that is equal to 8
 console.log("Addition: " + additionResult);
-let subtractionResult: number = 12 - 4;
+let subtractionResult: number = 12 - 4; //Created a variable of type number named as "subtractionResult" and stored a number
 console.log("Subtraction: " + subtractionResult);
-let multiplicationResult = 4 * 2;
+let multiplicationResult = 4 * 2; //Created a variable of type number named as "multiplicationResult" and stored a number whose product is equal to 8
 console.log("Multiplication: " + multiplicationResult);
-let divisionResult: number = 16 / 2
+let divisionResult: number = 16 / 2 //Created a variable of type number named as "divisionResult" whose value is equal to 8
 console.log("Division: " + divisionResult);

71b417f013ef232ae748e3b21fba5f8421dc62fd
Muaz <muazgulfam7890@gmail.com>
2024-03-02 10:22:40 +0500

    Completed the project till ex16

--- TypeScript/Typescript and Nodejs Exercises/exercise15/app.ts
+++ TypeScript/Typescript and Nodejs Exercises/exercise15/app.ts
@@ -3,3 +3,13 @@ console.log("Hey " + peopleToInvite[0] + " I would like you to be my guest tonig
 console.log(peopleToInvite[1] + ", join me at the dinner tonight");
 console.log(peopleToInvite[2] + "! join me at the dinner tonight");
 console.log(peopleToInvite[3] + ", join me at the dinner tonight");
+
+var isAbleToCome : boolean = false;
+if(isAbleToCome){
+    console.log(peopleToInvite[3] + " join me at the dinner tonight");
+}
+else{
+    console.log(peopleToInvite[3] + "wouldn't be able to make it to the dinner");
+    peopleToInvite.push("Anas");
+    console.log(peopleToInvite[4] + " Join me at the dinner tonight!");
+}
---
+++ TypeScript/Typescript and Nodejs Exercises/exercise16/app.ts
@@ -0,0 +1,25 @@
+var peopleToInvite = ["Habib", "Saad", "Taimoor", "Zeeshan"];
+console.log("Hey " + peopleToInvite[0] + " I would like you to be my guest tonight");
+console.log(peopleToInvite[1] + ", join me at the dinner tonight!");
+console.log(peopleToInvite[2] + "! join me at the dinner tonight!");
+console.log(peopleToInvite[3] + ", join me at the dinner tonight!");
+
+var isAbleToCome : boolean = false;
+if(isAbleToCome){
+    console.log(peopleToInvite[4] + " join me at the dinner tonight!");
+}
+else{
+    console.log(peopleToInvite[4] + " wouldn't be able to make it to the dinner");
+    peopleToInvite.push("Anas");
+    console.log(peopleToInvite[5] + " Join me at the dinner tonight!");
+}
+
+console.log("\nAlas! I have found a bigger dinner table so I would be inviting 3 more guest\n");
+
+peopleToInvite.splice(0, 0, "Hammad");
+peopleToInvite.splice(2, 0, "Asim");
+peopleToInvite.push("Fahad");
+
+for(var i = 0; i < peopleToInvite.length; i++){
+    console.log("Dear " + peopleToInvite[i] + ", Join me at the dinner tonight!")
+}

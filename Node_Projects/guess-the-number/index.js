4359e1f2991e9ba7d5e171ae3f605574bea12400
Muaz <muazgulfam7890@gmail.com>
2024-04-17 12:30:06 +0500

    Created a Number guessing Game

---
+++ Node_Projects/guess-the-number/index.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const generatedNum = Math.floor(Math.random() * 5);
+//console.log(generatedNum);
+let isCorrect = false;
+while (!isCorrect) {
+    const result = await inquirer.prompt([
+        {
+            name: "guessedNum",
+            type: "number",
+            message: "Guess a number between 0 and 5:"
+        }
+    ]);
+    if (result.guessedNum === generatedNum) {
+        console.log("Congrats! You guessed it right");
+        isCorrect = true;
+    }
+    else {
+        console.log("Better luck next time! Try again");
+    }
+}

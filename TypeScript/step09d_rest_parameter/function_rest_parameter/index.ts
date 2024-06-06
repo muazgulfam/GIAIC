d85cf86b27d686e69410fe53bbd82f6a139e076d
Muaz <muazgulfam7890@gmail.com>
2024-06-06 16:31:33 +0500

    Rest parameter completed

---
+++ TypeScript/step09d_rest_parameter/function_rest_parameter/index.ts
@@ -0,0 +1,35 @@
+/*let a: string = "Hello";
+console.log(typeof(a));*/
+
+function b(...info: any[]){
+    let stringArr: string[] = ["String not passed as an argument"];
+    let numberArr: number[] = [0];
+    let j = 0;
+    let k = 0;
+    for(let i = 0; i < info.length; i++){
+        if(typeof info[i] === "string"){
+            stringArr[j] = info[i] 
+            j++;
+        }
+        if(typeof info[i] === "number"){
+            numberArr[k] = info[i] 
+            k++
+        }
+    }
+    return "String: " + stringArr + "\nNumber: " + numberArr
+}
+
+console.log(b("Hello", "World", 2, 5, 7, "Hello again", 6, 9, 4, "World again"));
+//console.log(b(2, 5, 7, "Hello again", 6, 9, 4, "World agin"));
+
+
+
+/*let c :any[] = [1,2,3, "Hello"];
+console.log(typeof(c[0]));
+console.log(typeof(c[3]));
+if(typeof c[3] === "string"){
+    console.log("s");
+}
+if(typeof c[0] === "number"){
+    console.log("n");
+}*/
